27c195b7b0c22b89068deb9897f8a2a6d81f63e1
sindetisarah <sindetisara@gmail.com>
2021-06-13 18:44:17 +0300

    made some changes

--- app/src/main/java/com/example/nmbersrecylerview/MainActivity.kt
+++ app/src/main/java/com/example/nmbersrecylerview/MainActivity.kt
@@ -1,24 +1,33 @@
 package com.example.nmbersrecylerview
 
+import android.icu.math.BigDecimal
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import java.math.BigInteger
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        fibonacci(100)
+
+        var rvNumbers=findViewById<RecyclerView>(R.id.rvNumbers)
+
+        rvNumbers.layoutManager=
+            LinearLayoutManager(baseContext,)
+        var numbersAdapter=NumbersRecyclerViewAdapter(fibonacci(maxNumber=100))
+        rvNumbers.adapter=numbersAdapter
 
 
     }
-    fun fibonacci(maxNumber:Int):MutableList<Int>{
+}
+    fun fibonacci(maxNumber:Int):MutableList<BigInteger>{
         var count=0;
-        var firstNumber=0
-        var secondNumber=1
-        var sum = 0
-        var fibNumbers= mutableListOf<Int>()
+        var firstNumber=0.toBigInteger()
+        var secondNumber=1.toBigInteger()
+        var sum = 0.toBigInteger()
+        var fibNumbers= mutableListOf<BigInteger>()
 
         while(count<=maxNumber){
             println(firstNumber)
@@ -26,15 +35,10 @@ class MainActivity : AppCompatActivity() {
             firstNumber = secondNumber
             secondNumber = sum
             count ++
-            fibNumbers.plus(firstNumber)
+            fibNumbers.add(firstNumber)
         }
         return fibNumbers
-        var rvNumbers=findViewById<RecyclerView>(R.id.rvNumbers)
 
-        rvNumbers.layoutManager=
-            LinearLayoutManager(baseContext,)
-        var numbersAdapter=NumbersRecyclerViewAdapter(fibNumbers)
-        rvNumbers.adapter=numbersAdapter
 
     }
-    }
+
--- app/src/main/java/com/example/nmbersrecylerview/NumbersRecyclerViewAdapter.kt
+++ app/src/main/java/com/example/nmbersrecylerview/NumbersRecyclerViewAdapter.kt
@@ -4,11 +4,10 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import java.math.BigInteger
 
 
-
-
-class NumbersRecyclerViewAdapter(var numbersList:List<Int>):RecyclerView.Adapter<NumbersViewHolder>(){
+class NumbersRecyclerViewAdapter(var numbersList:MutableList<BigInteger>):RecyclerView.Adapter<NumbersViewHolder>(){
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): NumbersViewHolder {
         val itemView=
             LayoutInflater.from(parent.context).inflate(R.layout.number_list_view,parent,false)
